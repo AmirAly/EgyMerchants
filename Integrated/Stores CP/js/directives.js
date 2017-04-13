01b96a27a2a73a4b553844236902ea6852869385
shaymaa-saeed <shaymaa@appout.co>
2017-04-13 11:26:33 +0200

    edits

--- Integrated/Stores CP/js/directives.js
+++ Integrated/Stores CP/js/directives.js
@@ -111,7 +111,7 @@ app.directive('fallbacksrc', function () {
         link: function postLink(scope, iElement, iAttrs) {
             iElement.bind('error', function () {
                 angular.element(this).attr("src", 'images/down.png');
-                angular.element(this).attr("style", 'background-color:#fff;border-radius: 50%;');
+                angular.element(this).attr("style", 'background-color:#fff');
             });
         }
     }
