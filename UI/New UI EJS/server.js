4634092a1bfbd0147b3dbfbb5d19e0ae2ca7cfae
shaymaa-saeed <shaymaa@appout.co>
2017-04-23 17:10:26 +0200

    ui ejs

---
+++ UI/New UI EJS/server.js
@@ -0,0 +1,54 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// set path for static
+app.use(express.static(__dirname + '/public'));
+
+// use res.render to load up an ejs view file
+// index page 
+app.get('/', function (req, res) {
+    //var drinks = [
+    //    { name: 'Juice', drunkness: 3 },
+    //    { name: 'Tea', drunkness: 5 },
+    //    { name: 'Coffee', drunkness: 10 }
+    //];
+    //var tagline = "Hi I'm Samar.";
+
+    //var user = { name: 'samar', age: 28 };
+
+    res.render('pages/index', {
+        //drinks: drinks,
+        //tagline: tagline,
+        //user:user
+    });
+    //res.render('pages/index');
+});
+
+// store page 
+app.get('/store', function(req, res) {
+    res.render('pages/store');
+});
+
+// product page 
+app.get('/product', function (req, res) {
+    res.render('pages/product');
+});
+
+
+// items page 
+app.get('/items', function (req, res) {
+    res.render('pages/items');
+});
+
+
+// contacts page 
+app.get('/contacts', function (req, res) {
+    res.render('pages/contacts');
+});
+app.listen(8080);
+console.log('8080 is the magic port');
