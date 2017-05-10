573b9de5d1f94856e1752b1ce4bf270caf5c83f6
samarbakr <eng.samar.bakr@gmail.com>
2017-05-10 15:37:08 +0200

    UI

---
+++ UI/Market/js/general/customjcarousel.js
@@ -0,0 +1,20 @@
+﻿var productWidget = $(".products-widget");
+productWidget.each(function () {
+    var jcarousetItemsNumber = $(this).find("ul li").size();
+    var scrollItems = 3;
+    var autoTime = 0;
+    if (jcarousetItemsNumber > 3) {
+        if ($(this).hasClass('autoscroll')) { scrollItems = 1; autoTime = 3; }
+        $(this).jcarousel({
+            vertical: true,
+            items: 'ul.slides li',
+            scroll: scrollItems,
+            wrap: 'circular',
+            auto: autoTime,
+            animation: "slow",
+            easing: "swing",
+            buttonNextHTML: '<a></a>',
+            buttonPrevHTML: '<a></a>'
+        });
+    }
+})
