060c86773d42887f5ffdf783217e77fb6e67ed8d
Amir Aly <Amir Aly>
2017-05-15 14:59:01 +0200

    Fix port issue

--- Integrated/Market/server.js
+++ Integrated/Market/server.js
@@ -46,7 +46,7 @@ mongoose.connect(db.url, function (err) {
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept , authorization");
             next();
         });
-        app.listen(8080);
+        app.listen(80);
         app.get('*', function (req, res) {
             return res.redirect('/eg/Home');
         });
