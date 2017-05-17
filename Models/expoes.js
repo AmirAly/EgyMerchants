598fd63c3bc67a481865540fd465ee428924552c
anisaibrahimappout <anisaibrahimappout@gmail.com>
2017-05-17 11:42:51 +0200

    sections paging

--- Models/expoes.js
+++ Models/expoes.js
@@ -11,11 +11,40 @@ module.exports = {
                     reject('1:' + err);
                 else {
                     if (lst.length > 0) {
-                        resolve(lst);
+                        var finalLst = [];
+                        for (var i = 0 ; i < lst.length; i++) {
+                            var obj = lst[i];
+                            obj.floors = lst[i].Sections.length / 25;
+                            obj.Sections = obj.Sections.splice(0, 25);
+                            finalLst.push({
+                                _id: obj._id,
+                                Title: obj.Title,
+                                Banner: obj.Banner,
+                                Sections: obj.Sections,
+                                Floors: obj.floors
+                            });
+                            resolve(finalLst);
+                        }
                     }
                     else {
                         reject("This filteration didn't resulted in any data");
                     }
+                }
+            })
+        })
+    },
+    getSectionsByFloor: function (_storeId, _floor) {
+        return new Promise(function (resolve, reject) {
+            var skippedSections = (_floor - 1) * 25;
+            Schema.findOne({ '_id': _storeId }, { _id: 1, Title: 1, Banner: 1, Sections: { $slice: [skippedSections, 25] } }).populate({
+                path: 'Sections.Store',
+                model: 'Store'
+            }).exec(function (err, Obj) {
+                if (err)
+                    reject('1:' + err);
+                else {
+                    if (Obj)
+                        resolve(Obj);
                 }
             })
         })
