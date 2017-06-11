1cf97db7b58074b4a596ba19a9c2912587c2263a
samarbakr <eng.samar.bakr@gmail.com>
2017-06-11 14:21:41 +0200

    login market UI

--- Integrated/Market/public/js/controllers/headercontroller.js
+++ Integrated/Market/public/js/controllers/headercontroller.js
@@ -1,4 +1,68 @@
 ﻿app.controller("headerController", function ($scope, $rootScope, $timeout) {
+
+    new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated'
+    }).init();
+
+    jQuery(function ($) {
+        $(document).ready(function () {
+            checkScrollTop();
+            $(window).scroll(function () {
+                checkScrollTop();
+            });
+            function checkScrollTop() {
+                if ($(window).scrollTop() < 100) {
+                    $('.header-wrapper').attr('style', 'position: static !important;'); // .removeClass('isScrolled');
+                }
+                else {
+                    $('.header-wrapper').attr('style', 'position: fixed !important;').addClass('isScrolled');
+                }
+            }
+
+        });
+    });
+
+    //$rootScope.$on('$stateChangeSuccess', function () {
+    //    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    //});
+    $scope.loginForm = true;
+    $scope.afterLoginError = "";
+    $scope.loginObj = {};
+    $scope.submitLogin = function (form) {
+        $scope.afterLoginError = "";
+        if (form.$valid) {
+            // call loader , login , hide modal & add user name
+            $scope.dataLoading = true;
+            console.log($scope.loginObj);
+            $timeout(function () {
+                $scope.dataLoading = false;
+                $scope.frmLogin.$setPristine();
+                $scope.frmLogin.$setUntouched();
+                $scope.loginObj = {};
+                $scope.dismiss();
+            }, 500);
+        }
+    }
+
+    $scope.afterRegisterError = "";
+    $scope.registerObj = {};
+    $scope.submitRegister = function (form) {
+        $scope.afterRegisterError = "";
+        if (form.$valid) {
+            // call loader , Register , hide modal & add user name
+            $scope.dataLoading = true;
+            console.log($scope.registerObj);
+            $timeout(function () {
+                $scope.dataLoading = false;
+                form.$setPristine();
+                form.$setUntouched();
+                $scope.registerObj = {};
+                $scope.dismiss();
+            }, 500);
+        }
+    }
+
     $scope.txtSearch = '';
     $scope.search = function () {
         if ($scope.txtSearch != '') {
