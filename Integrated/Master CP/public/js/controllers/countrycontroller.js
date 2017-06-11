8b8a580a1a7de53781da96c7a5b765242aefa78d
shaymaa-saeed <shaymaa@appout.co>
2017-06-11 09:52:01 +0200

    int

--- Integrated/Master CP/public/js/controllers/countrycontroller.js
+++ Integrated/Master CP/public/js/controllers/countrycontroller.js
@@ -1,2 +1,16 @@
 ﻿egm.controller("countryController", function ($scope, API) {
+    var req = {
+        method: 'put',
+        url: '/Country/Edit',
+        data: {
+            Name: $scope.country.Name,
+            IsoCode: $scope.country.IsoCode,
+            Flag: '',
+            WelcomeMsg: $scope.country.WelcomeMsg,
+            Status: "Active"
+        }
+    }
+    API.execute(req).then(function (_res) {
+        console.log(_res);
+    });
 });
