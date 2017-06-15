452aeaf46d7d61a3be203ba416f8c2fa169e215a
shaymaa-saeed <shaymaa@appout.co>
2017-06-15 15:03:53 +0200

    mr

--- Integrated/Market/public/js/warehouse.js
+++ Integrated/Market/public/js/warehouse.js
@@ -1,6 +1,6 @@
 app.factory('API', function ($http) {
     var _url = 'http://localhost:8007';
-    //var _url = 'http://egm-app.azurewebsites.net';
+    ///var _url = 'http://egm-app.azurewebsites.net';
     return {
         name: 'API',
         execute: function (_req) {
