e8bfd9ee6fbf23ead47aa9c16cc1f7fa6c6ee226
anisaibrahimappout <anisaibrahimappout@gmail.com>
2017-07-31 17:31:23 +0200

    update

--- API/app/notifications.js
+++ API/app/notifications.js
@@ -1,6 +1,26 @@
 var Schema = require('./models/notification');
 module.exports = {
     updateStatus: function (_id) {
-        return new Promise(function (resolve, reject) { })
+        return new Promise(function (resolve, reject) {
+            Schema.findOneAndUpdate({ '_id':_id }, { $set: { 'Status': "read" } }, { new: true }, function (err, Obj) {
+                if (err)
+                    reject({
+                        code: 1,
+                        data: err
+                    })
+                else {
+                    if(Obj)
+                        resolve({
+                            code: 100,
+                            data: "Notification status changed successfully"
+                        });
+                    else
+                        reject({
+                            code: 21,
+                            data: "This filteration didn't resulted in any data"
+                        })
+                }
+            })
+        })
     }
 }
