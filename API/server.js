7bb563a8711f59672bfe328ffe1e6ee45b298312
elmonshare7 <a.elmonshareh@appout.co>
2017-10-18 11:09:09 +0200

    elmonshare7

--- API/server.js
+++ API/server.js
@@ -287,7 +287,7 @@ mongoose.connect(db.url, function (err) {
 // }, function (err) {
 //    console.log(err);
 // });
-// store.getById("595b80f374be602d00a76050").then(function (result) {
+// store.getById("59e715769bc6940012ef7d91").then(function (result) {
 //    console.log(result);
 // }, function (err) {
 //    console.log(err);
@@ -561,13 +561,13 @@ mongoose.connect(db.url, function (err) {
 //       });
 
 
-item.getSimilar(
+// item.getSimilar(
 
-   "59e4c98e0c7eba32d43f7858"
+//    "59e4c98e0c7eba32d43f7858"
    
-  ).then(function (result) {
-          console.log(result);
-      }, function (err) {
-          console.log(err);
-      });
+//   ).then(function (result) {
+//           console.log(result);
+//       }, function (err) {
+//           console.log(err);
+//       });
     
